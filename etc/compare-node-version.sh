455dfef57767bc78e3347ac06c227811c863afbb
Jozef Celuch <jozef.celuch@gmail.com>
2024-08-20 17:19:13 +0200

    Update required node version to 20

--- etc/compare-node-version.sh
+++ etc/compare-node-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nodever="$(node --version)"
 currentver="${nodever:1}"
-requiredver="18.0.0"
+requiredver="20.0.0"
 if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
   exit 0
 else
